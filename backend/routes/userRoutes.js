06d3cfcad9802918bbe1b2ae32b7d7fa424f10dd
Udara Ireshan <udaraireshan@gmail.com>
2024-11-30 00:58:05 +0530

    up

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { register } = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
-
-const router = express.Router();
-router.post("/register", register);
-
-module.exports = router;
--- generateSecret.js
+++
@@ -1,5 +0,0 @@
-const crypto = require('crypto');
-
-// Generate a 64-byte random key (hex format)
-const secretKey = crypto.randomBytes(64).toString('hex');
-console.log(secretKey);
