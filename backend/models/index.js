b0d15a80e21b85d393474f33cf64137eb41c8d81
Udara Ireshan <udaraireshan@gmail.com>
2024-11-30 00:09:56 +0530

    del

--- backend/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("../config/db");
-const User = require("./userModel");
-const Ticket = require("./ticketModel");
-
-sequelize.sync({ alter: true }) // Sync models to database
-  .then(() => console.log("Models synced to database"))
-  .catch((err) => console.log("Error syncing models: " + err));
-
-module.exports = { User, Ticket };
