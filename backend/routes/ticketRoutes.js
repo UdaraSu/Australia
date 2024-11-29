eba232b5100b334c10826b2105596cbe4bc0e469
Udara Ireshan <udaraireshan@gmail.com>
2024-11-30 01:19:31 +0530

    del

--- backend/routes/ticketRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createTicket, getTickets } = require("../controllers/ticketController");
-const { protect } = require("../middleware/authMiddleware");
-
-const router = express.Router();
-router.post("/create", protect, createTicket);
-router.get("/", protect, getTickets);
-
-module.exports = router;
